0ddc1559bca1fe38ce7ef2ff18ceec307f94e044
Briancoughlin <76997526+Briancoughlin@users.noreply.github.com>
2021-01-11 17:36:07 +0000

    Sidebar.js formatinng for clarity

--- sidebars.js
+++ sidebars.js
@@ -21,16 +21,36 @@ module.exports =
       },
         'mlapi-basics/object-spawning', 
         'mlapi-basics/object-behavior-relation',
-         'mlapi-basics/modularity', 
-         'mlapi-basics/networkedvar', 
-         'mlapi-basics/syncedvar', 
-         'mlapi-basics/scene-management',
-         'mlapi-basics/object-ownership', 
-         'mlapi-basics/object-visibility',
-         'mlapi-basics/logging'
+        'mlapi-basics/modularity', 
+        'mlapi-basics/networkedvar', 
+        'mlapi-basics/syncedvar', 
+        'mlapi-basics/scene-management',
+        'mlapi-basics/object-ownership', 
+        'mlapi-basics/object-visibility',
+        'mlapi-basics/logging'
     ],
-    'Core Components': ['core-components/core-components', 'core-components/networkingmanager', 'core-components/trackedobject', 'core-components/networkedobject', 'core-components/networkedbehavior'],
-    'Advanced Topics': ['advanced-topics/message-encryption', 'advanced-topics/object-pooling', 'advanced-topics/lag-compensation', 'advanced-topics/bitwriter-bitreader-bitstream', 'advanced-topics/custom-transports', 'advanced-topics/networkprofiler-window', 'advanced-topics/custom-serialization', 'advanced-topics/boxing-systems'],
-    'Troubleshooting': ['troubleshooting/troubleshooting'],
+    'Core Components': 
+    [
+      'core-components/core-components',
+      'core-components/networkingmanager', 
+      'core-components/trackedobject', 
+      'core-components/networkedobject', 
+      'core-components/networkedbehavior'
+    ],
+    'Advanced Topics': 
+    [
+      'advanced-topics/message-encryption',
+      'advanced-topics/object-pooling', 
+      'advanced-topics/lag-compensation', 
+      'advanced-topics/bitwriter-bitreader-bitstream', 
+      'advanced-topics/custom-transports', 
+      'advanced-topics/networkprofiler-window', 
+      'advanced-topics/custom-serialization', 
+      'advanced-topics/boxing-systems'
+    ],
+    'Troubleshooting': 
+    [
+      'troubleshooting/troubleshooting'
+    ],
   },
 };
