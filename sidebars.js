039a70da8960a4a2718246fa2d8c3cb515a044db
Briancoughlin <76997526+Briancoughlin@users.noreply.github.com>
2021-01-12 14:32:29 +0000

    completion of the RPC skeleton Document

--- sidebars.js
+++ sidebars.js
@@ -7,36 +7,63 @@ module.exports =
       'about-mlapi',
       'getting-started/installation', 
       'getting-started/library-initialization', 
-      'getting-started/connection-approval'
+      'getting-started/connection-approval',
     ],
     'MLAPI Basics': 
-    [
+    [    
       {
         'Messaging System': 
         [
           'mlapi-basics/introduction', 
-          'mlapi-basics/ClientRpc',
-          'mlapi-basics/ServerRpc',
-        ]
-      },
-        'mlapi-basics/object-spawning', 
-        'mlapi-basics/object-behavior-relation',
-        'mlapi-basics/modularity', 
-        'mlapi-basics/networkedvar', 
-        'mlapi-basics/syncedvar', 
-        'mlapi-basics/scene-management',
-        'mlapi-basics/object-ownership', 
-        'mlapi-basics/object-visibility',
-        'mlapi-basics/logging'
+          {
+            'RPC Remote Procedure Call':
+
+            [
+              'mlapi-basics/clientrpc',
+              'mlapi-basics/serverrpc',
+              'mlapi-basics/reliability',
+              'mlapi-basics/execution-table',
+              'mlapi-basics/rpc-params',
+
+              
+                {
+                'Serialization':
+                  [
+                  'mlapi-basics/serialization-intro'
+                  ],
+                },
+              
+              'mlapi-basics/backward-compatibility',
+              'mlapi-basics/cross-compatibility',
+              'mlapi-basics/deprecation-of-return-values',
+             
+            ],  
+          },
+                   
+        ],
+
+      },      
+
+    'mlapi-basics/object-spawning', 
+    'mlapi-basics/object-behavior-relation',
+    'mlapi-basics/modularity', 
+    'mlapi-basics/networkedvar', 
+    'mlapi-basics/syncedvar', 
+    'mlapi-basics/scene-management',
+    'mlapi-basics/object-ownership', 
+    'mlapi-basics/object-visibility',
+    'mlapi-basics/logging',
     ],
+
     'Core Components': 
     [
       'core-components/core-components',
       'core-components/networkingmanager', 
       'core-components/trackedobject', 
       'core-components/networkedobject', 
-      'core-components/networkedbehavior'
+      'core-components/networkedbehavior',
     ],
+
     'Advanced Topics': 
     [
       'advanced-topics/message-encryption',
@@ -46,11 +73,13 @@ module.exports =
       'advanced-topics/custom-transports', 
       'advanced-topics/networkprofiler-window', 
       'advanced-topics/custom-serialization', 
-      'advanced-topics/boxing-systems'
+      'advanced-topics/boxing-systems',
     ],
+
     'Troubleshooting': 
     [
-      'troubleshooting/troubleshooting'
+      'troubleshooting/troubleshooting',
     ],
+
   },
 };
