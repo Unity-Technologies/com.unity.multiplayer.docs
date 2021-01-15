b4677f290caf6ef2b1fb0920e7a8e88b4208abb5
Lori Krell <lori.krell@unity3d.com>
2021-01-15 12:54:30 -0600

    Update Sidebars across branches

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,156 @@
+/*
+* This file creates the left navigation table of contents for the site.
+* types: category (section of content), doc (markdown file), ref (markdown page, no navigation), link (webpage)
+* id: id of the markdown file, includes subdirectory names
+*/
 module.exports = {
-  docs: {
-    'Getting Started': ['about-mlapi', 'getting-started/installation', 'getting-started/library-initialization', 'getting-started/connection-approval'],
-    'MLAPI Basics': ['mlapi-basics/messaging-system', 'mlapi-basics/object-spawning', 'mlapi-basics/object-behavior-relation', 'mlapi-basics/modularity', 'mlapi-basics/networkedvar', 'mlapi-basics/syncedvar', 'mlapi-basics/scene-management', 'mlapi-basics/object-ownership', 'mlapi-basics/object-visibility', 'mlapi-basics/logging'],
-    'Core Components': ['core-components/core-components', 'core-components/networkingmanager', 'core-components/trackedobject', 'core-components/networkedobject', 'core-components/networkedbehavior'],
-    'Advanced Topics': ['advanced-topics/message-encryption', 'advanced-topics/object-pooling', 'advanced-topics/lag-compensation', 'advanced-topics/bitwriter-bitreader-bitstream', 'advanced-topics/custom-transports', 'advanced-topics/networkprofiler-window', 'advanced-topics/custom-serialization', 'advanced-topics/boxing-systems'],
-    'Troubleshooting': ['troubleshooting/troubleshooting'],
+  mlapi: [
+  {
+    "collapsed": true,
+    "type": "category",
+    "label": "Getting Started",
+    "items": [
+      {
+        "type": "doc",
+        "id": "about-mlapi"
+      },
+      {
+        "type": "doc",
+        "id": "getting-started/installation"
+      },
+      {
+        "type": "doc",
+        "id": "getting-started/library-initialization"
+      },
+      {
+        "type": "doc",
+        "id": "getting-started/connection-approval"
+      }
+    ]
   },
+  {
+    "collapsed": true,
+    "type": "category",
+    "label": "MLAPI Basics",
+    "items": [
+      {
+        "type": "doc",
+        "id": "mlapi-basics/messaging-system"
+      },
+      {
+        "type": "doc",
+        "id": "mlapi-basics/object-spawning"
+      },
+      {
+        "type": "doc",
+        "id": "mlapi-basics/object-behavior-relation"
+      },
+      {
+        "type": "doc",
+        "id": "mlapi-basics/modularity"
+      },
+      {
+        "type": "doc",
+        "id": "mlapi-basics/networkedvar"
+      },
+      {
+        "type": "doc",
+        "id": "mlapi-basics/syncedvar"
+      },
+      {
+        "type": "doc",
+        "id": "mlapi-basics/scene-management"
+      },
+      {
+        "type": "doc",
+        "id": "mlapi-basics/object-ownership"
+      },
+      {
+        "type": "doc",
+        "id": "mlapi-basics/object-visibility"
+      },
+      {
+        "type": "doc",
+        "id": "mlapi-basics/logging"
+      }
+    ]
+  },
+  {
+    "collapsed": true,
+    "type": "category",
+    "label": "Core Components",
+    "items": [
+      {
+        "type": "doc",
+        "id": "core-components/core-components"
+      },
+      {
+        "type": "doc",
+        "id": "core-components/networkingmanager"
+      },
+      {
+        "type": "doc",
+        "id": "core-components/trackedobject"
+      },
+      {
+        "type": "doc",
+        "id": "core-components/networkedobject"
+      },
+      {
+        "type": "doc",
+        "id": "core-components/networkedbehavior"
+      }
+    ]
+  },
+  {
+    "collapsed": true,
+    "type": "category",
+    "label": "Advanced Topics",
+    "items": [
+      {
+        "type": "doc",
+        "id": "advanced-topics/message-encryption"
+      },
+      {
+        "type": "doc",
+        "id": "advanced-topics/object-pooling"
+      },
+      {
+        "type": "doc",
+        "id": "advanced-topics/lag-compensation"
+      },
+      {
+        "type": "doc",
+        "id": "advanced-topics/bitwriter-bitreader-bitstream"
+      },
+      {
+        "type": "doc",
+        "id": "advanced-topics/custom-transports"
+      },
+      {
+        "type": "doc",
+        "id": "advanced-topics/networkprofiler-window"
+      },
+      {
+        "type": "doc",
+        "id": "advanced-topics/custom-serialization"
+      },
+      {
+        "type": "doc",
+        "id": "advanced-topics/boxing-systems"
+      }
+    ]
+  },
+  {
+    "collapsed": true,
+    "type": "category",
+    "label": "Troubleshooting",
+    "items": [
+      {
+        "type": "doc",
+        "id": "troubleshooting/troubleshooting"
+      }
+    ]
+  }
+]
 };
