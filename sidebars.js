f3d6ac5f601851b59de713fcb8e0134a09671b22
s-omeilia-unity <89528471+s-omeilia-unity@users.noreply.github.com>
2021-10-20 17:52:52 -0400

    How to install tools

--- sidebars.js
+++ sidebars.js
@@ -21,6 +21,10 @@ module.exports = {
       },
       {
         "type": "doc",
+        "id": "tools/install-tools",
+      },
+      {
+        "type": "doc",
         "id": "migration/upgrade-guide",
       },
       {
