537c4daa88addac828ebb57c7e23e6d63c0d5782
s-omeilia-unity <89528471+s-omeilia-unity@users.noreply.github.com>
2022-04-05 15:29:42 -0400

    Build error fix attempt

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -309,7 +309,7 @@ module.exports = {
               return `https://github.com/Unity-Technologies/com.unity.multiplayer.docs/edit/main/${versionDocsDirPath}/${docPath}`;
             },
             includeCurrentVersion: true,
-            lastVersion: '1.0.0',
+            lastVersion: "current",
             versions: {
               current: {
                 label: '1.0.0',
@@ -477,7 +477,7 @@ module.exports = {
 
           },
           includeCurrentVersion: true,
-            lastVersion: `1.0.0`,
+            lastVersion: `current`,
             versions: {
               current: {
                 label: '1.0.0',
