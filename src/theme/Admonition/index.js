49a1ca47a6039047db819ac598cd695c7bd7a269
Anton Iancu <anton.iancu@unity3d.com>
2023-03-08 17:00:52 -0500

    fixes missing closing tags in func definition ...and xlmns related build error

--- src/theme/Admonition/index.js
+++ src/theme/Admonition/index.js
@@ -1,30 +1,103 @@
-import React from 'react';
-import clsx from 'clsx';
-import {ThemeClassNames} from '@docusaurus/theme-common';
-import Translate from '@docusaurus/Translate';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import { ThemeClassNames } from "@docusaurus/theme-common";
+import Translate from "@docusaurus/Translate";
+import styles from "./styles.module.css";
 
 function UnityIcon() {
   return (
-    <svg width="25px" height="24px" viewBox="0 0 507 506" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="unity-logo-black" fill="#757575" fill-rule="nonzero"><path d="M451.009018,0.49163636 L244.575273,54.2842182 L214.027491,106.713018 L152.028945,106.261527 L0.92378182,253.214836 L152.028945,400.135564 L213.994909,399.684073 L244.612509,452.080291 L451.009018,505.872873 L506.305018,305.192145 L474.886836,253.214836 L506.305018,201.237527 L451.009018,0.49163636 Z M218.998545,115.151709 L376.908655,75.6811636 L286.261382,228.387491 L104.943564,228.387491 L218.998545,115.151709 Z M218.998545,391.240727 L104.943564,278.0096 L286.261382,278.0096 L376.908655,430.711273 L218.998545,391.240727 Z M421.098909,405.888582 L330.419055,253.214836 L421.098909,100.475927 L464.884218,253.214836 L421.098909,405.888582 L421.098909,405.888582 Z" id="Fill-1"></path></g></g></svg>                            
-  );
-function FaqIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z"/></svg>
-  );
-function BestPracticeIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="18px" height="18px"><path d="M0 0h24v24H0z" fill="none"/><path d="M23 12l-2.44-2.78.34-3.68-3.61-.82-1.89-3.18L12 3 8.6 1.54 6.71 4.72l-3.61.81.34 3.68L1 12l2.44 2.78-.34 3.69 3.61.82 1.89 3.18L12 21l3.4 1.46 1.89-3.18 3.61-.82-.34-3.68L23 12zm-10 5h-2v-2h2v2zm0-4h-2V7h2v6z"/></svg>
-  );
-function ContributionIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="36px" height="36px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zm3.23 15.39L12 15.45l-3.22 1.94c-.38.23-.85-.11-.75-.54l.85-3.66-2.83-2.45c-.33-.29-.15-.84.29-.88l3.74-.32 1.46-3.45c.17-.41.75-.41.92 0l1.46 3.44 3.74.32c.44.04.62.59.28.88l-2.83 2.45.85 3.67c.1.43-.36.77-.74.54z" fill="#855EF0"/></svg>
-  );
-function FunFactIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><g><rect fill="none" height="24" width="24" x="0" y="0"/></g><g><path d="M20,2H4C2.9,2,2,2.9,2,4v18l4-4h14c1.1,0,2-0.9,2-2V4C22,2.9,21.1,2,20,2z M13.57,11.57L12,15l-1.57-3.43L7,10l3.43-1.57 L12,5l1.57,3.43L17,10L13.57,11.57z"/></g></svg>
+    <svg
+      width="25px"
+      height="24px"
+      viewBox="0 0 507 506"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g
+        id="Page-1"
+        stroke="none"
+        stroke-width="1"
+        fill="none"
+        fill-rule="evenodd"
+      >
+        <g id="unity-logo-black" fill="#757575" fill-rule="nonzero">
+          <path
+            d="M451.009018,0.49163636 L244.575273,54.2842182 L214.027491,106.713018 L152.028945,106.261527 L0.92378182,253.214836 L152.028945,400.135564 L213.994909,399.684073 L244.612509,452.080291 L451.009018,505.872873 L506.305018,305.192145 L474.886836,253.214836 L506.305018,201.237527 L451.009018,0.49163636 Z M218.998545,115.151709 L376.908655,75.6811636 L286.261382,228.387491 L104.943564,228.387491 L218.998545,115.151709 Z M218.998545,391.240727 L104.943564,278.0096 L286.261382,278.0096 L376.908655,430.711273 L218.998545,391.240727 Z M421.098909,405.888582 L330.419055,253.214836 L421.098909,100.475927 L464.884218,253.214836 L421.098909,405.888582 L421.098909,405.888582 Z"
+            id="Fill-1"
+          ></path>
+        </g>
+      </g>
+    </svg>
   );
 }
+function FaqIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="black"
+      width="24px"
+      height="24px"
+    >
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z" />
+    </svg>
+  );
+}
+
+function BestPracticeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="black"
+      width="18px"
+      height="18px"
+    >
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M23 12l-2.44-2.78.34-3.68-3.61-.82-1.89-3.18L12 3 8.6 1.54 6.71 4.72l-3.61.81.34 3.68L1 12l2.44 2.78-.34 3.69 3.61.82 1.89 3.18L12 21l3.4 1.46 1.89-3.18 3.61-.82-.34-3.68L23 12zm-10 5h-2v-2h2v2zm0-4h-2V7h2v6z" />
+    </svg>
+  );
+}
+
+function ContributionIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="black"
+      width="36px"
+      height="36px"
+    >
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path
+        d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zm3.23 15.39L12 15.45l-3.22 1.94c-.38.23-.85-.11-.75-.54l.85-3.66-2.83-2.45c-.33-.29-.15-.84.29-.88l3.74-.32 1.46-3.45c.17-.41.75-.41.92 0l1.46 3.44 3.74.32c.44.04.62.59.28.88l-2.83 2.45.85 3.67c.1.43-.36.77-.74.54z"
+        fill="#855EF0"
+      />
+    </svg>
+  );
+}
+
+function FunFactIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      enable-background="new 0 0 24 24"
+      viewBox="0 0 24 24"
+      fill="black"
+      width="24px"
+      height="24px"
+    >
+      <g>
+        <rect fill="none" height="24" width="24" x="0" y="0" />
+      </g>
+      <g>
+        <path d="M20,2H4C2.9,2,2,2.9,2,4v18l4-4h14c1.1,0,2-0.9,2-2V4C22,2.9,21.1,2,20,2z M13.57,11.57L12,15l-1.57-3.43L7,10l3.43-1.57 L12,5l1.57,3.43L17,10L13.57,11.57z" />
+      </g>
+    </svg>
+  );
+}
+
 function NoteIcon() {
   return (
     <svg viewBox="0 0 14 16">
@@ -35,6 +108,7 @@ function NoteIcon() {
     </svg>
   );
 }
+
 function TipIcon() {
   return (
     <svg viewBox="0 0 12 16">
@@ -45,6 +119,7 @@ function TipIcon() {
     </svg>
   );
 }
+
 function DangerIcon() {
   return (
     <svg viewBox="0 0 12 16">
@@ -55,6 +130,7 @@ function DangerIcon() {
     </svg>
   );
 }
+
 function InfoIcon() {
   return (
     <svg viewBox="0 0 14 16">
@@ -65,6 +141,7 @@ function InfoIcon() {
     </svg>
   );
 }
+
 function CautionIcon() {
   return (
     <svg viewBox="0 0 16 16">
@@ -75,114 +152,125 @@ function CautionIcon() {
     </svg>
   );
 }
+
 // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
 const AdmonitionConfigs = {
   unity: {
-    infimaClassName: 'secondary',
+    infimaClassName: "secondary",
     iconComponent: UnityIcon,
     label: (
       <Translate
         id="theme.admonition.unity"
-        description="The default label used for the Unity admonition (:::unity)">
+        description="The default label used for the Unity admonition (:::unity)"
+      >
         note
       </Translate>
     ),
   },
   faq: {
-    infimaClassName: 'secondary',
+    infimaClassName: "secondary",
     iconComponent: FaqIcon,
     label: (
       <Translate
         id="theme.admonition.faq"
-        description="The default label used for the FAQ admonition (:::faq)">
+        description="The default label used for the FAQ admonition (:::faq)"
+      >
         note
       </Translate>
     ),
   },
   bestpractice: {
-    infimaClassName: 'secondary',
+    infimaClassName: "secondary",
     iconComponent: BestPracticeIcon,
     label: (
       <Translate
         id="theme.admonition.bestpractice"
-        description="The default label used for the BestPractice admonition (:::bestpractice)">
+        description="The default label used for the BestPractice admonition (:::bestpractice)"
+      >
         note
       </Translate>
     ),
   },
   funfact: {
-    infimaClassName: 'secondary',
+    infimaClassName: "secondary",
     iconComponent: FunFactIcon,
     label: (
       <Translate
         id="theme.admonition.funfact"
-        description="The default label used for the FunFact admonition (:::funfact)">
+        description="The default label used for the FunFact admonition (:::funfact)"
+      >
         note
       </Translate>
     ),
   },
   contribution: {
-    infimaClassName: 'secondary',
+    infimaClassName: "secondary",
     iconComponent: ContributionIcon,
     label: (
       <Translate
         id="theme.admonition.contribution"
-        description="The default label used for the Contribution admonition (:::contribution)">
+        description="The default label used for the Contribution admonition (:::contribution)"
+      >
         note
       </Translate>
     ),
   },
   note: {
-    infimaClassName: 'secondary',
+    infimaClassName: "secondary",
     iconComponent: NoteIcon,
     label: (
       <Translate
         id="theme.admonition.note"
-        description="The default label used for the Note admonition (:::note)">
+        description="The default label used for the Note admonition (:::note)"
+      >
         note
       </Translate>
     ),
   },
   tip: {
-    infimaClassName: 'success',
+    infimaClassName: "success",
     iconComponent: TipIcon,
     label: (
       <Translate
         id="theme.admonition.tip"
-        description="The default label used for the Tip admonition (:::tip)">
+        description="The default label used for the Tip admonition (:::tip)"
+      >
         tip
       </Translate>
     ),
   },
   danger: {
-    infimaClassName: 'danger',
+    infimaClassName: "danger",
     iconComponent: DangerIcon,
     label: (
       <Translate
         id="theme.admonition.danger"
-        description="The default label used for the Danger admonition (:::danger)">
+        description="The default label used for the Danger admonition (:::danger)"
+      >
         danger
       </Translate>
     ),
   },
   info: {
-    infimaClassName: 'info',
+    infimaClassName: "info",
     iconComponent: InfoIcon,
     label: (
       <Translate
         id="theme.admonition.info"
-        description="The default label used for the Info admonition (:::info)">
+        description="The default label used for the Info admonition (:::info)"
+      >
         info
       </Translate>
     ),
   },
   caution: {
-    infimaClassName: 'warning',
+    infimaClassName: "warning",
     iconComponent: CautionIcon,
     label: (
       <Translate
         id="theme.admonition.caution"
-        description="The default label used for the Caution admonition (:::caution)">
+        description="The default label used for the Caution admonition (:::caution)"
+      >
         caution
       </Translate>
     ),
@@ -190,10 +278,10 @@ const AdmonitionConfigs = {
 };
 // Legacy aliases, undocumented but kept for retro-compatibility
 const aliases = {
-  secondary: 'note',
-  important: 'info',
-  success: 'tip',
-  warning: 'danger',
+  secondary: "note",
+  important: "info",
+  success: "tip",
+  warning: "danger",
 };
 function getAdmonitionConfig(unsafeType) {
   const type = aliases[unsafeType] ?? unsafeType;
@@ -202,7 +290,7 @@ function getAdmonitionConfig(unsafeType) {
     return config;
   }
   console.warn(
-    `No admonition config found for admonition type "${type}". Using Info as fallback.`,
+    `No admonition config found for admonition type "${type}". Using Info as fallback.`
   );
   return AdmonitionConfigs.info;
 }
@@ -212,8 +300,7 @@ function extractMDXAdmonitionTitle(children) {
   const items = React.Children.toArray(children);
   const mdxAdmonitionTitle = items.find(
     (item) =>
-      React.isValidElement(item) &&
-      item.props?.mdxType === 'mdxAdmonitionTitle',
+      React.isValidElement(item) && item.props?.mdxType === "mdxAdmonitionTitle"
   );
   const rest = <>{items.filter((item) => item !== mdxAdmonitionTitle)}</>;
   return {
@@ -222,7 +309,9 @@ function extractMDXAdmonitionTitle(children) {
   };
 }
 function processAdmonitionProps(props) {
-  const {mdxAdmonitionTitle, rest} = extractMDXAdmonitionTitle(props.children);
+  const { mdxAdmonitionTitle, rest } = extractMDXAdmonitionTitle(
+    props.children
+  );
   return {
     ...props,
     title: props.title ?? mdxAdmonitionTitle,
@@ -230,20 +319,26 @@ function processAdmonitionProps(props) {
   };
 }
 export default function Admonition(props) {
-  const {children, type, title, icon: iconProp} = processAdmonitionProps(props);
+  const {
+    children,
+    type,
+    title,
+    icon: iconProp,
+  } = processAdmonitionProps(props);
   const typeConfig = getAdmonitionConfig(type);
   const titleLabel = title ?? typeConfig.label;
-  const {iconComponent: IconComponent} = typeConfig;
+  const { iconComponent: IconComponent } = typeConfig;
   const icon = iconProp ?? <IconComponent />;
   return (
     <div
       className={clsx(
         ThemeClassNames.common.admonition,
         ThemeClassNames.common.admonitionType(props.type),
-        'alert',
+        "alert",
         `alert--${typeConfig.infimaClassName}`,
-        styles.admonition,
-      )}>
+        styles.admonition
+      )}
+    >
       <div className={styles.admonitionHeading}>
         <span className={styles.admonitionIcon}>{icon}</span>
         {titleLabel}
