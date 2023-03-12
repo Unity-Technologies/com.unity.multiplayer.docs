ad3c9d3573c2dda15506b84c13d58219e39f0cd6
Anton Iancu <anton.iancu@unity3d.com>
2023-03-11 21:09:07 -0500

    fixes navbar and mermaid graphs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,4 +1,4 @@
-const navbar = require("./config/navbar.config.js");
+// const navbar = require("./config/navbar.config.js");
 const admonitions = require("./config/admonitions.config.js");
 const scripts = require("./config/scripts.config.js");
 const i18n = require("./config/i18n.config.js");
@@ -14,7 +14,14 @@ module.exports = {
   organizationName: "unity", // Usually your GitHub org/user name.
   projectName: "docusaurus", // Usually your repo name.
   scripts,
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
   themeConfig: {
+    mermaid: {
+      theme: { light: "neutral", dark: "dark" },
+    },
     docs: {
       sidebar: {
         hideable: true,
@@ -48,7 +55,6 @@ module.exports = {
       disableSwitch: false,
       respectPrefersColorScheme: true,
     },
-    navbar,
     // Code block themes and languages
     prism: {
       theme: require("prism-react-renderer/themes/vsLight"),
@@ -139,6 +145,196 @@ module.exports = {
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Unity Technologies. Built with Docusaurus.`,
     },
+    navbar: {
+      title: "Multiplayer Networking",
+      logo: {
+        alt: "Unity",
+        src: "img/logo.png",
+      },
+      items: [
+        // Netcode for GameObjects Dropdown
+        {
+          label: "Netcode for GameObjects",
+          // className: 'nav-break',
+          type: "dropdown",
+          docId: "about",
+          position: "left",
+          items: [
+            {
+              type: "docSidebar",
+              sidebarId: "releases",
+              label: "Release Notes",
+              docsPluginId: "releases",
+            },
+            {
+              type: "doc",
+              docId: "about",
+              label: "Documentation",
+            },
+            {
+              type: "doc",
+              docId: "api/introduction",
+              label: "API Reference",
+            },
+          ],
+        },
+        // Netcode for Gameobjects Versions Dropdown
+        {
+          type: "docsVersionDropdown",
+          position: "left",
+          // Add additional dropdown items at the beginning/end of the dropdown.
+          //dropdownItemsBefore: [],
+          // dropdownItemsAfter: [{to: '/versions', label: 'All versions'}],
+          // Do not add the link active class when browsing docs.
+          dropdownActiveClassDisabled: true,
+          docsPluginId: "default",
+        },
+        // Transport Dropdown
+        {
+          type: "dropdown",
+          docId: "about",
+          label: "Transport",
+          position: "left",
+          docsPluginId: "transport",
+          className: "nav-break",
+          items: [
+            {
+              type: "doc",
+              docId: "transport/transport-1-0-0",
+              label: "Release Notes",
+              docsPluginId: "releases",
+            },
+            {
+              type: "doc",
+              docId: "about",
+              label: "Documentation",
+              docsPluginId: "transport",
+            },
+
+            {
+              type: "doc",
+              docId: "api/introduction",
+              label: "API Reference",
+              docsPluginId: "transport",
+            },
+          ],
+        },
+        // Transport Versions Dropdown
+        {
+          type: "docsVersionDropdown",
+          position: "left",
+          dropdownActiveClassDisabled: true,
+          docsPluginId: "transport",
+        },
+        // Multiplayer Tools Dropdown
+        {
+          to: "install-tools",
+          label: "Multiplayer Tools",
+          position: "left",
+          docsPluginId: "tools",
+          className: "nav-break",
+          items: [
+            {
+              type: "doc",
+              docId: "tools/tools-1-0-0",
+              label: "Release Notes",
+              docsPluginId: "releases",
+            },
+            {
+              type: "doc",
+              docId: "install-tools",
+              label: "Documentation",
+              docsPluginId: "tools",
+            },
+            {
+              type: "doc",
+              docId: "api/introduction",
+              label: "API Reference",
+              docsPluginId: "tools",
+            },
+          ],
+        },
+        // Multiplayer Tools Versions Dropdown
+        {
+          type: "docsVersionDropdown",
+          position: "left",
+          dropdownActiveClassDisabled: true,
+          docsPluginId: "tools",
+        },
+        {
+          className: "navbar-github-link",
+          position: "right",
+          to: "contribute",
+          docsPluginId: "reference",
+          items: [
+            {
+              to: "/introduction",
+              label: "Release Notes",
+              docsPluginId: "releases",
+            },
+            {
+              label: "Docs Repo",
+              href: "https://github.com/Unity-Technologies/com.unity.multiplayer.docs",
+            },
+            {
+              label: "Netcode for GameObjects Repo",
+              href: "https://github.com/Unity-Technologies/com.unity.netcode.gameobjects",
+            },
+            {
+              label: "Multiplayer Community Contributions",
+              href: "https://github.com/Unity-Technologies/multiplayer-community-contributions",
+            },
+            {
+              label: "Boss Room Repo",
+              href: "https://github.com/Unity-Technologies/com.unity.multiplayer.samples.coop",
+            },
+            {
+              label: "Bitesize Samples Repo",
+              href: "https://github.com/Unity-Technologies/com.unity.multiplayer.samples.bitesize",
+            },
+            {
+              label: "Contribution Guide",
+              href: "https://github.com/Unity-Technologies/com.unity.multiplayer.docs/wiki",
+            },
+            {
+              type: "doc",
+              docId: "template",
+              label: "Markdown Template",
+              docsPluginId: "reference",
+            },
+          ],
+        },
+        {
+          position: "right",
+          className: "navbar-grid-menu",
+          to: "contribute",
+          docsPluginId: "reference",
+          items: [
+            { href: "/blog", label: "Blog" },
+            {
+              label: "Product Roadmap",
+              href: "https://unity.com/roadmap/unity-platform/multiplayer-networking",
+            },
+            {
+              label: "Discord",
+              href: "https://discord.gg/buMxnnPvTb",
+            },
+            {
+              label: "Unity Multiplayer Forum",
+              href: "https://forum.unity.com/forums/multiplayer.26/",
+            },
+            {
+              label: "Netcode for GameObjects Forum",
+              href: "https://forum.unity.com/forums/netcode-for-gameobjects.661/",
+            },
+            {
+              label: "Unity Transport Forum",
+              href: "https://forum.unity.com/forums/unity-transport.664/",
+            },
+          ],
+        },
+      ],
+    },
   },
   presets: [
     [
@@ -148,7 +344,7 @@ module.exports = {
         docs: {
           routeBasePath: "netcode",
           sidebarPath: require.resolve("./sidebars.js"),
-          remarkPlugins: [require("mdx-mermaid")],
+          // remarkPlugins: [require("mdx-mermaid")],
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
           // Edit URL for version
