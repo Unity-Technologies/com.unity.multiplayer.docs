10451a1f150af47463b515acaf8f7234da51614d
Anton Iancu <anton.iancu@unity3d.com>
2023-03-15 04:46:46 -0400

    updates admonitions config list

--- config/admonitions.config.js
+++ config/admonitions.config.js
@@ -1,4 +1,4 @@
-//Admonitions component is swizzled (ejected) to impleent custom types and icons. See `src/theme/Admonitions/index.js` for implementations details.
+//Admonitions component is swizzled (wrapped) to implement custom types and icons. See `src/theme/Admonitions/index.js` for implementations details.
 const admonitions = {
   tag: ":::",
   keywords: [
@@ -7,11 +7,17 @@ const admonitions = {
     "bestpractice", // custom
     "faq", // custom
     "unity", // custom
-    "note",
-    "tip",
-    "info",
-    "caution",
-    "danger",
+
+    "caution", // built-in
+    "note", // built-in
+    "tip", // built-in
+    "danger", //built-in
+    "info", // built-in
+
+    // "secondary", // deprecated, not used
+    // "success", // deprecated, not used
+    "warning", // deprecated, but in use
+    "important", // deprecated, but in use
   ],
 };
 
