863d5425cb667e4ea2cbaee709244b081579b9e0
Anton Iancu <anton.iancu@unity3d.com>
2023-04-24 13:23:12 -0400

    fix styling for docsfeedback links

--- src/components/DocItem/DocsFeedbackLinks.js
+++ src/components/DocItem/DocsFeedbackLinks.js
@@ -14,50 +14,48 @@ function DocsFeedbackLinks() {
   const mdPath = /(?<=main\/).*/.exec(editUrl)[0];
 
   return (
-    <div className="row">
-      <div className="row margin-left--none navbar__inner liner">
-        <div className="user-options">
-          {/* PDF Button*/}
-          <div
-            className="margin-right--md pointer display-flex"
-            style={{ display: "flex" }}
-          >
-            {editUrl && (
-              <a onClick={() => window.print()}>
-                <FontAwesomeIcon icon={faPrint} />
-                PDF
-              </a>
-            )}
-          </div>
+    <div className="">
+      <div className="user-options">
+        {/* PDF Button*/}
+        <div
+          className="margin-right--md pointer display-flex"
+          style={{ display: "flex" }}
+        >
+          {editUrl && (
+            <a onClick={() => window.print()}>
+              <FontAwesomeIcon icon={faPrint} />
+              PDF
+            </a>
+          )}
+        </div>
 
-          {/* Edit URL */}
-          <div>
-            {editUrl && (
-              <a href={editUrl} target="_blank" rel="noreferrer noopener">
-                <IconEdit />
-                Edit this page
-              </a>
-            )}
-          </div>
+        {/* Edit URL */}
+        <div className="margin-right--md pointer display-flex">
+          {editUrl && (
+            <a href={editUrl} target="_blank" rel="noreferrer noopener">
+              <IconEdit />
+              Edit this page
+            </a>
+          )}
+        </div>
 
-          {/* Open Doc Button*/}
-          <div
-            className="margin-right--md display-flex"
-            style={{ display: "flex" }}
-          >
-            {mdPath && (
-              <a
-                href={
-                  "https://github.com/Unity-Technologies/com.unity.multiplayer.docs/issues/new?labels=feedback&title=Feedback%20for%20" +
-                  mdPath
-                }
-                target="_blank"
-              >
-                <IconBug />
-                Log an issue
-              </a>
-            )}
-          </div>
+        {/* Open Doc Button*/}
+        <div
+          className="margin-right--md pointer display-flex"
+          style={{ display: "flex" }}
+        >
+          {mdPath && (
+            <a
+              href={
+                "https://github.com/Unity-Technologies/com.unity.multiplayer.docs/issues/new?labels=feedback&title=Feedback%20for%20" +
+                mdPath
+              }
+              target="_blank"
+            >
+              <IconBug />
+              Log an issue
+            </a>
+          )}
         </div>
       </div>
     </div>
