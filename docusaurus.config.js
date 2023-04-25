66101e01507de2a8a1a0abe569a4981c574f4fb9
s-omeilia-unity <89528471+s-omeilia-unity@users.noreply.github.com>
2023-04-25 11:05:46 -0400

    Docusaurus config update for locale

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,5 @@
 const admonitions = require("./config/admonitions.config.js");
 const scripts = require("./config/scripts.config.js");
-const i18n = require("./config/i18n.config.js");
 
 module.exports = {
   title: "Unity Multiplayer Networking",
@@ -503,5 +502,13 @@ module.exports = {
     "@saucelabs/theme-github-codeblock",
     "react-iframe",
   ],
-  i18n,
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+    localeConfigs: {
+      en: {
+        htmlLang: "en-US",
+      },
+    },
+  },
 };
