64bdbae9d9e46a6305dae3859c65fc400ac64a75
Alex Martin <alexander.martin@unity3d.com>
2023-11-01 16:22:59 -0700

    Styling the legal footer links. NOTE: I do not like the use of IMPORTANT however this was necessary to override the default OneTrust styling that gets injected after our own custom styles. I'm open to an alternate way to fix this.

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -9,6 +9,7 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import { useThemeConfig } from '@docusaurus/theme-common';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import OneTrust from "@site/static/svg/onetrust.svg";
 import styles from './styles.module.scss';
 
 function FooterLink({
@@ -88,7 +89,13 @@ function Footer() {
             <li><a href="https://unity.com/legal/privacy-policy">Privacy Policy </a></li>
             <li><a href="https://unity.com/legal/cookie-policy">Cookies</a></li>
             <li><a href="https://unity.com/legal/do-not-sell-my-personal-information">Do Not Sell or Share My Personal Information</a></li>
-            <li><a href="javascript:void(0);" id="ot-sdk-btn" class="ot-sdk-show-settings">Your Privacy Choices (Cookie Settings)</a></li>
+            <li>
+                <a href="javascript:void(0);" id="ot-sdk-btn" class="ot-sdk-show-settings">
+                  <OneTrust />
+                  &nbsp;
+                  Your Privacy Choices (Cookie Settings)
+                </a>
+              </li>
          </ul>
         </div>
         <div className="footer__details" >"Unity", Unity logos, and other Unity trademarks are trademarks or registered trademarks of Unity Technologies or its affiliates in the U.S. and elsewhere (<a href="https://unity.com/legal/trademarks">more info here</a>). Other names or brands are trademarks of their respective owners.</div>
