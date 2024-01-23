e531865f6de0a85cd06f3d6679f32f0e93af5922
Amy Reeve <amy.reeve@unity3d.com>
2024-01-23 12:56:41 +0000

    Bumping MPPM to 1.0.0 pre

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -365,8 +365,12 @@ module.exports = {
         lastVersion: "current",
         versions: {
           current: {
-            label: "0.6.0",
+            label: "1.0.0-pre.2",
             path: "current",
+          },
+          "0.6.0": {
+            label: "0.6.0",
+            path: "0.6.0",
           },
           "0.5.0": {
             label: "0.5.0",
