f1ff8a48f3550580cb025fac159e0c3adc2bce9a
Amy Reeve <amy.reeve@unity3d.com>
2024-01-24 13:50:02 +0000

    Update sidebarsTools.js

--- sidebarsTools.js
+++ sidebarsTools.js
@@ -26,7 +26,7 @@ module.exports = {
         {
             "type": "link",
             "label": "API Reference",
-            "href": "https://docs.unity3d.com/Packages/com.unity.multiplayer.tools@2.2/api/index.html",
+            "href": "https://docs.unity3d.com/Packages/com.unity.multiplayer.tools@2.1/api/index.html",
         },
         {
             "type": "doc",
