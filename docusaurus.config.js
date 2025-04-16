f486b5bf21b1cfa19498034e91837d496ea3fad8
Amy Reeve <amy.reeve@unity3d.com>
2025-04-16 12:57:19 +0100

    Removing unsupported MPPM versions (#1463)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -412,22 +412,6 @@ module.exports = {
             label: "1.0.0",
             path: "1.0.0",
           },
-          "0.6.0": {
-            label: "0.6.0",
-            path: "0.6.0",
-          },
-          "0.5.0": {
-            label: "0.5.0",
-            path: "0.5.0",
-          },
-          "0.4.0": {
-            label: "0.4.0",
-            path: "0.4.0",
-          },
-          "0.3.0": {
-            label: "0.3.0",
-            path: "0.3.0",
-          },
         },
         editCurrentVersion: true,
         routeBasePath: "mppm",
